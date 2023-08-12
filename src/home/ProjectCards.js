89a7ea1c12f4a58976d5c7eed898baf5e36bd895
Abdus Sattar <sasorganicagro@gmail.com>
2023-08-13 02:49:33 +0600

    Add home page Card with next button

--- src/home/ProjectCards.js
+++ src/home/ProjectCards.js
@@ -2,73 +2,64 @@ import React, { useState } from 'react';
 import { Button } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import projectInfo from '../data/projectInfo.json';
+import './home.css';
 
-function ProjectCards() {
+const ProjectCards = () => {
   const navigate = useNavigate();
   const handleSeeMore = () => {
     navigate('/recent_work');
   };
 
-  const [currentGroupIndex, setCurrentGroupIndex] = useState(0);
+  const [currentProjectIndex, setCurrentProjectIndex] = useState(0);
 
   if (!projectInfo || projectInfo.length === 0) {
-    return <div>No doctors found.</div>;
+    return <div>No projects found.</div>;
   }
 
-  const groupSize = 3;
-  const totalGroups = Math.ceil(projectInfo.length / groupSize);
-
-  const handleNextGroup = () => {
-    setCurrentGroupIndex((prevIndex) => (prevIndex + 1) % totalGroups);
+  const handleNextProject = () => {
+    setCurrentProjectIndex((prevIndex) => (prevIndex + 1) % projectInfo.length);
   };
 
-  const handlePrevGroup = () => {
-    setCurrentGroupIndex((prevIndex) => (prevIndex - 1 + totalGroups) % totalGroups);
+  const handlePrevProject = () => {
+    setCurrentProjectIndex((prevIndex) => (prevIndex === 0
+      ? projectInfo.length - 1 : prevIndex - 1));
   };
 
-  setTimeout(() => {
-    handleNextGroup();
-  }, 19000);
+  const currentProject = projectInfo[currentProjectIndex];
 
   return (
-    <>
-      <div className="d-flex justify-content-between align-items-center m-0 p-0">
-        <button
-          type="button"
-          className="btn btn-primary rounded-end-pill"
-          onClick={handlePrevGroup}
-          disabled={currentGroupIndex === 0}
-        >
-          Prev
-        </button>
-        <div className="card-group w-100">
-          {projectInfo
-            .slice(currentGroupIndex * groupSize, (currentGroupIndex + 1) * groupSize)
-            .map((project) => (
-              <div key={project.id} className="card d-flex align-items-center">
-                <img src={project.image} alt={project.name} />
-                <div className="card-body">
-                  <p className="card-title">{project.name}</p>
-                  <p className="card-text">{project.link}</p>
-                </div>
-              </div>
-            ))}
+    <div className="d-flex justify-content-between align-items-center m-0 p-0">
+      <button
+        type="button"
+        className="btn btn-primary rounded-end-pill"
+        onClick={handlePrevProject}
+      >
+        Prev
+      </button>
+      <div className="card project-card">
+        <div className="card-overlay">
+          <h5 className="card-title">{currentProject.title}</h5>
+          <p className="card-text">{currentProject.description}</p>
+          <div className="d-flex flex-column align-items-center">
+            <Button onClick={handleSeeMore}>See More</Button>
+          </div>
         </div>
-
-        <button
-          type="button"
-          className="btn btn-primary rounded-start-pill"
-          onClick={handleNextGroup}
-          disabled={currentGroupIndex === totalGroups - 1}
-        >
-          Next
-        </button>
+        <img
+          src={currentProject.image}
+          alt={currentProject.title}
+          className="card-img-top img-fluid"
+          style={{ maxHeight: '400px', objectFit: 'cover' }}
+        />
       </div>
-      <div className="d-flex flex-column align-items-center">
-        <Button onClick={handleSeeMore}>See More </Button>
-      </div>
-    </>
+      <button
+        type="button"
+        className="btn btn-primary rounded-start-pill"
+        onClick={handleNextProject}
+      >
+        Next
+      </button>
+    </div>
   );
-}
+};
 
 export default ProjectCards;
